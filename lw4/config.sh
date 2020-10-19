9711d1c86be0fe29f1394e000ce8e70a3d65497d
Nick <nick@home.home>
2020-10-19 15:51:58 -0800

    LW 4

---
+++ lw4/config.sh
@@ -0,0 +1,12 @@
+export REDIS_PORT=6379
+export REDIS_HOST=redis
+
+export NATS_PORT=4222
+export NATS_HOST=nats
+export NATS_BUS=events
+
+export BACKEND_PROTOCOL=http
+export BACKEND_HOST=backendapi
+export BACKEND_PORT=5000
+
+export FRONTEND_PORT=80
